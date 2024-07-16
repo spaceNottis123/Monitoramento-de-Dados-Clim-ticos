a7c51d66ba5e89a7441be8edafbd1f98a8ea58a7
Enzo Trevisan <enzotrevisan.sk8@gmail.com>
2024-07-16 02:00:02 -0300

    adding comments on main.py

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify
 import logging
+from logging.handlers import RotatingFileHandler
 from Controller.api import get_weather
-
-
-logging.basicConfig(
-    level=logging.DEBUG,  # Set the desired logging level
-    format="%(asctime)s - %(levelname)s - %(message)s",  # Customize the format
-    handlers=[logging.StreamHandler()]  # Use a StreamHandler to write to the console (optional)
-)
-logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+print("Setting up logging...")
+
+if not app.debug:
+    handler = RotatingFileHandler('api.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+
+    # Set the logger level to INFO
+    app.logger.setLevel(logging.INFO)
+    print("Logging set up completed")
+
+
 @app.route("/api/weather/<city>")
 def get_current_weather(city):
-    #logger.debug(f"Fetching weather data for city: {city}")
-    logger.info(f"Successfully retrieved weather data for city: {city}")
-    return get_weather(city)
-
+    app.logger.info(f"Successfully retrieved weather data for city: {city}")
+    try:
+        weather_data = get_weather(city)
+        app.logger.info(f"Successfully retrieved weather data for city: {city}")
+        return jsonify(weather_data)
+    except Exception as e:
+        app.logger.error(f"Error retrieving weather data for city: {city}. Error: {e}")
+        return jsonify({"error": "Unable to retrieve weather data"}), 500
 
 if __name__ == '__main__':
-    logger.info(f"Starting API")
-    app.run(debug=False)
-    logger.info(f"Finish API")
+    app.logger.info("Starting API")
+    try:
+        app.run(debug=False)
+    except Exception as e:
+        app.logger.error(f"Error starting the API: {e}")
+    finally:
+        app.logger.info("Stopping API")
+
